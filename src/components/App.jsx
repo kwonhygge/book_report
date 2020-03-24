058389ff0f0b30fefafbf38a40e656fff8406117
kwonhygge <kitteniso1103@gmail.com>
2020-03-24 16:59:58 +0900

    Make roof for noteList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import NoteList from "./NoteList"
 
 function App(){
     return(
         <div>
             <Header />
+            <NoteList />
         </div>
     )
 }
