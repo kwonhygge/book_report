f32637ac4a36eb6a0b3c40b8ebe3641e91a1f900
kwonhygge <kitteniso1103@gmail.com>
2020-04-16 17:39:41 +0900

    Add notemake component

--- src/components/NoteMake/NoteMake.jsx
+++ src/components/NoteMake/NoteMake.jsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
 import "./NoteMake.css";
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+import {Link} from "react-router-dom";
+import 'react-markdown-editor-lite/lib/index.css';
 
-function NoteMake(){
+function NoteMake(props){
+    const [note,setNote] = useState({
+        title:"",
+        content:""
+    });
+
+    const mdParser = new MarkdownIt();
+
+    function submitNote(event){
+        localStorage.setItem(note.title,note.content);
+        event.preventDefault();
+      }
+
+    function handleInputChange(event){
+        const {name,value} = event.target;
+    
+        setNote((prevValue)=>{
+          return{
+            ...prevValue,
+            [name]:value
+          };
+        });
+    
+      }
+
+    function handleEditorChange({html}) { 
+        setNote((prevValue)=>{
+            return{
+                ...prevValue,
+                ["content"]:html
+            }
+        })
+      }
+
     return (
         <div>
-        <input></input>
-        <textarea></textarea>
-    </div>
+        <form>
+            <button onClick={submitNote}>click</button>
+            <input id="title" name="title" onChange={handleInputChange} placeholder="책 제목"></input>
+            <MdEditor
+                value="# 느낀 점을 작성해주세요."
+                renderHTML={(text) => mdParser.render(text)}
+                style={{height:"400px"}}
+                onChange={handleEditorChange}
+            />
+        
+        </form>
+        
+        </div>
     )
 }
 
