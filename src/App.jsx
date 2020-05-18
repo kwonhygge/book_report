8e2fe44fe2492565a1a3f2896b930388c3fea203
kwonhygge <kitteniso1103@gmail.com>
2020-05-18 19:51:23 +0900

    Revert "Notelist css"
    This reverts commit 029e39e46eed1abfd1c7ae81e683342ff6fe986c.

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import {NoteContainer} from "./containers";
 import propTypes from "prop-types";
 
 function App(){
-    
-    return(  
+    return(
         <div>
             <Header />
             <NoteContainer />
