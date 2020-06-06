61deb479277c4506cb20aaaf4f1a6d33eba2fcc8
kwonhygge <kitteniso1103@gmail.com>
2020-06-06 15:56:29 +0900

    Reverting stage

--- src/components/NoteWrapper/NoteWrapper.js
+++ src/components/NoteWrapper/NoteWrapper.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./NoteWrapper.css";
 
 function NoteWrapper({children}){
-    console.log({children});
     return(
         <div className="noteWrapper">
             {children}
